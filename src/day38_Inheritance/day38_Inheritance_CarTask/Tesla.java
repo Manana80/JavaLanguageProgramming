a2fe93ca8499efd7b32af03e7ef286145d10480b
Manana80 <93110108+Manana80@users.noreply.github.com>
2022-01-13 11:47:43 -0500

    day38: Inheritance continue: method overriding

--- src/day38_Inheritance/day38_Inheritance_CarTask/Tesla.java
+++ src/day38_Inheritance/day38_Inheritance_CarTask/Tesla.java
@@ -1,11 +1,26 @@
 package day38_Inheritance.day38_Inheritance_CarTask;
 
 public class Tesla extends Car{
-    public Tesla(String brand, String model, int year, double price, String color, int miles) {
-        super(brand, model, year, price, color, miles);
+    public Tesla( String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
     }
 
     public void autopilot(){
         System.out.println(brand + " " + model + " is in autopilot mood" );
     }
 }
+
+
+
+/*
+IS A RELATIONSHIP one class extends another class
+- Tester extends Employee
+- Tiger extends Animal
+- Nokia and Samsung extend Phone
+HAS A RELATIONSHIP one class contains another class, but does not inherit or pass fields
+- ScrumTeam has a Tester
+- Zoo has a tiger
+- Car has Engine
+- Phone shop has Nokia and Samsung phones
+
+ */
---
+++ src/day38_Inheritance/day38_Inheritance_CarTask/parkingLot.java
@@ -0,0 +1,7 @@
+package day38_Inheritance.day38_Inheritance_CarTask;
+
+public class parkingLot {
+    Toyota toyota = new Toyota("Camry",2020,20000,"Grey",123456);
+    Tesla tesla = new Tesla("Model S", 2022,94990,"Red",0);
+    BMW bmw = new BMW("X5", 2020, 67350, "black", 12000);
+}
