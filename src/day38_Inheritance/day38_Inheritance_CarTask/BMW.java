423f8cab44b5e5aa07f3342b773e607b74e729c1
Manana80 <93110108+Manana80@users.noreply.github.com>
2022-01-13 11:01:01 -0500

    day38: Inheritance continue: method overriding

--- src/day38_Inheritance/day38_Inheritance_CarTask/BMW.java
+++ src/day38_Inheritance/day38_Inheritance_CarTask/BMW.java
@@ -1,16 +1,16 @@
 package day38_Inheritance.day38_Inheritance_CarTask;
 
 public class BMW extends Car{
-    public BMW(String brand, String model, int year, double price, String color, int miles) {
-        super(brand, model, year, price, color, miles);
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
     }
 
 
     public void breaksDown(){
-        System.out.println("breaks down");
+        System.out.println(brand + " " + model + " breaks down every 100 miles");
     }
    public void  racing(){
-       System.out.println( "The " + color + " " + brand + " " + model + " is racing");
+       System.out.println( "The " + color + " " + brand + " " + model + " is a racing car");
    }
 
 
