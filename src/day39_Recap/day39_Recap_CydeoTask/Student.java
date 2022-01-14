a09874d4ffe92f6c355238aaa15b8223eca60797
Manana80 <93110108+Manana80@users.noreply.github.com>
2022-01-14 16:19:31 -0500

    Cydeo Task , Student class is completed!

---
+++ src/day39_Recap/day39_Recap_CydeoTask/Student.java
@@ -0,0 +1,56 @@
+package day39_Recap.day39_Recap_CydeoTask;
+/*
+ 6. Create a sub class of Person named Student
+
+            Extra variables:
+                studentId, fieldOfStudy
+
+            Encapsulate all the fields
+
+            Add a constructor to set all the fields
+
+            Encapsulate all the fields
+
+            Methods:
+                study()
+ */
+public class Student extends Person{
+    private int studentID;
+    private String fieldOfStudy;
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
+
+    public void setFieldOfStudy(String fieldOfStudy) {
+        this.fieldOfStudy = fieldOfStudy;
+    }
+
+    public Student(String name, int age, char gender, int studentID, String fieldOfStudy) {
+        super(name, age, gender);
+        setStudentID(studentID);
+        setFieldOfStudy(fieldOfStudy);
+    }
+
+    public void study(){
+        System.out.println(getName() + getStudentID() + " is studying " + getFieldOfStudy());
+    }
+
+    public String toString() {
+        return "Student{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                ", studentID=" + studentID +
+                ", fieldOfStudy='" + fieldOfStudy + '\'' +
+                '}';
+    }
+}
