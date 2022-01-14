4c7401f81abb946fde8709d944a7270478a99ac2
Manana80 <93110108+Manana80@users.noreply.github.com>
2022-01-14 10:57:52 -0500

    day39: Shape (parent class is completed)

--- src/day39_Recap/day39_Recap_Shape/Shape.java
+++ src/day39_Recap/day39_Recap_Shape/Shape.java
@@ -3,6 +3,35 @@ package day39_Recap.day39_Recap_Shape;
 public class Shape {
     private String name;  // encapsulate and create getters and setters
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null){
+            System.err.println("Name can not be null");
+            System.exit(1); // 1: means something went wrong
+        }
+        if (name.isEmpty() || name.isBlank()){
+            System.err.println("Invalid name");
+            System.exit(1);
+        }
+
+
+        this.name = name;
+    }
+
+    public Shape(String name) {
+        setName(name);
+    }
+
+    public double area (){
+        return 0; // because Shape is a generic word and we do not have a formula for shape!!
+    }
+
+public double perimeter(){
+        return 0;
+}
 
 
 
