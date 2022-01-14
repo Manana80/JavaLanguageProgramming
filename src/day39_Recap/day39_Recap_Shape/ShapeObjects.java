fb40db9ee1f998745948bce9877176b17e22527e
Manana80 <93110108+Manana80@users.noreply.github.com>
2022-01-14 14:42:40 -0500

    Shape objects done

---
+++ src/day39_Recap/day39_Recap_Shape/ShapeObjects.java
@@ -0,0 +1,19 @@
+package day39_Recap.day39_Recap_Shape;
+
+public class ShapeObjects {
+    public static void main(String[] args) {
+
+        Square square = new Square(2.5);
+
+        System.out.println(square);
+
+        Rectangle rectangle = new Rectangle(5, 10);
+
+        System.out.println(rectangle);
+
+        Circle circle = new Circle(4.5);
+        System.out.println(circle);
+
+
+    }
+}
