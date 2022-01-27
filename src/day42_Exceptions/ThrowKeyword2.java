fd274aeaa0f43a23fc461988adbeada0f23bcc93
Manana80 <93110108+Manana80@users.noreply.github.com>
2022-01-27 18:37:36 -0500

    Exceptions done

---
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -0,0 +1,16 @@
+package day42_Exceptions;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+    public static void main(String[] args) {
+        throw new NoSuchElementException("There is no such element as break");
+
+        // System.out.println("hello world");
+
+        // throw new Rectangle(5,7); the object should only belong to the throwable class
+
+
+
+    }
+}
